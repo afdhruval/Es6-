a772c0bdc49ce56d80355e5c4aea1e9dbe93780b
afdhurval <dhruvaldalwadi1907@gmail.com>
2026-05-30 16:05:09 +0530

    feat: refactor movie management routes and views; streamline movie CRUD operations

--- Backend/MoviehUB/backend/src/controller/movieController.js
+++ Backend/MoviehUB/backend/src/controller/movieController.js
@@ -1,104 +1,6 @@
 import movieModel from "../models/movieModel.js";
 
-
-// CREATE MOVIE
-
-
-async function movieAdd(req, res) {
-  const { title, hero, cast, duration, description, imgUrl, trailerMp4 } =
-    req.body;
-
-  const movie = await movieModel.create({
-    title,
-    hero,
-    cast,
-    duration,
-    description,
-    imgUrl,
-    trailerMp4,
-  });
-
-  res.status(201).json({
-    message: "Movie Created",
-    movie,
-  });
-}
-
-
-// GET ALL MOVIES API
-
-
-async function getAllMoviesApi(req, res) {
-  const movies = await movieModel.find();
-
-  res.status(200).json({
-    message: "Movies fetched",
-    movies,
-  });
-}
-
-
-// GET SINGLE MOVIE API
-
-
-async function getMovieApi(req, res) {
-  const { id } = req.params;
-
-  const movie = await movieModel.findById(id);
-
-  res.status(200).json({
-    movie,
-  });
-}
-
-
-// UPDATE API
-
-
-async function updateMovie(req, res) {
-  const { id } = req.params;
-
-  const movie = await movieModel.findByIdAndUpdate(
-    id,
-    req.body,
-    {
-      new: true,
-    }
-  );
-
-  res.status(200).json({
-    message: "Movie Updated",
-    movie,
-  });
-}
-
-
-// 
-
-
-async function deleteMovie(req, res) {
-  const { id } = req.params;
-
-  await movieModel.findByIdAndDelete(id);
-
-  res.status(200).json({
-    message: "Movie Deleted",
-  });
-}
-
-
-// EJS HOME PAGE
-
-
-async function homePage(req, res) {
-  res.render("home");
-}
-
-
-// EJS ALL MOVIES PAGE
-
-
-async function moviesPage(req, res) {
+async function movieGetAll(req, res) {
   const movies = await movieModel.find();
 
   res.render("movies", {
@@ -106,42 +8,44 @@ async function moviesPage(req, res) {
   });
 }
 
+async function homePage(req, res) {
+  const movies = await movieModel.find();
 
-// EJS SINGLE MOVIE PAGE
+  res.render("home", {
+    movies,
+  });
+}
 
-
-async function moviePage(req, res) {
-  const { id } = req.params;
-
-  const movie = await movieModel.findById(id);
+async function movieGet(req, res) {
+  const movie = await movieModel.findById(req.params.id);
 
   res.render("movie", {
     movie,
   });
 }
 
-
-// EJS EDIT PAGE
-
-
 async function editMoviePage(req, res) {
-  const { id } = req.params;
-
-  const movie = await movieModel.findById(id);
+  const movie = await movieModel.findById(req.params.id);
 
   res.render("editMovie", {
     movie,
   });
 }
 
+async function updateMovie(req, res) {
+  await movieModel.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.redirect("/api/movie/movies");
+}
+
 export default {
-  movieAdd,
-  getAllMoviesApi,
-  getMovieApi,
-  updateMovie,
-  deleteMovie,
-  homePage,
-  moviesPage,
-  moviePage,
+  movieGetAll,
+  movieGet,
   editMoviePage,
+  updateMovie,
+  homePage,
 };
--- Backend/MoviehUB/backend/src/routes/movie.route.js
+++ Backend/MoviehUB/backend/src/routes/movie.route.js
@@ -3,24 +3,13 @@ import movieController from "../controller/movieController.js";
 
 const router = express.Router();
 
-// EJS Pages
 router.get("/", movieController.homePage);
-
-router.get("/movies", movieController.moviesPage);
-
-router.get("/movie/:id", movieController.moviePage);
-
+// Pages
+router.get("/movies", movieController.movieGetAll);
+router.get("/movie/:id", movieController.movieGet);
 router.get("/movie/edit/:id", movieController.editMoviePage);
 
-// APIs
-router.post("/add", movieController.movieAdd);
-
-router.get("/all", movieController.getAllMoviesApi);
-
-router.get("/:id", movieController.getMovieApi);
-
-router.put("/update/:id", movieController.updateMovie);
-
-router.delete("/delete/:id", movieController.deleteMovie);
+// Form Submit
+router.post("/update/:id", movieController.updateMovie);
 
 export default router;
